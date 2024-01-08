0855f30a415002ed7372ff341328e15b4fc1da00
shrihari237 <90607160+shrihari237@users.noreply.github.com>
2024-01-08 18:08:36 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,13 @@
+let words=[
+    {
+        "inputs":5,
+        "category":"Sports",
+        "word":"Chess"
+    },
+    {
+        "inputs":6,
+        "category":"European Country Name",
+        "word":"France"
+    },
+  
+]
